c7e1fc8b250424c60513fbfd69529031f9dfdd0f
晨曦中的暮雨 <3075514079@qq.com>
2025-07-10 18:40:11 +0800

    day09全部完成，但是和阿里云有关的集成部分是没有完成的，这一部分内容其实就是对上传内容部分的小改造而已

---
+++ tlias-web-management/src/main/java/com/yaojingxi/controller/UploadController.java
@@ -0,0 +1,27 @@
+package com.yaojingxi.controller;
+
+import com.yaojingxi.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Insert;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class UploadController {
+    @PostMapping("/upload")
+    public Result upload(String name, Integer age, MultipartFile  file) throws IOException {
+        log.info("上传文件：name={}，age={}，file={}",name,age,file);
+        // 保存文件名
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String filename = UUID.randomUUID().toString() + file.getOriginalFilename() +  extension;
+        // 转存文件
+        file.transferTo(new File("F:/home/yaoyao/TempDate_by_Program/"+filename));
+        return Result.success();
+    }
+}
